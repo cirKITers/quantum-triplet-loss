05fb67a7aab43393babf4048c556c34123eabf24
Christof Wendenius <christof.wendenius@kit.edu>
2021-11-30 09:45:37 +0100

    cleanup and decays

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 import pennylane as qml
-import matplotlib.pyplot as plt
 from pennylane import numpy as np
 from maskit.datasets import load_data
+from collections import Counter
+from utils import davies_bouldin_index, distances_between_centers, plot
+
 
 SEED = 1337
 np.random.seed(SEED)
@@ -11,20 +13,28 @@ random.seed(SEED)
 
 WIRES = 4
 LAYERS = 5
-STEPS = 5001
-ALPHA = 0.5
 TRAIN_SIZE = 2000
 TEST_SIZE = 400
+CLASSES = (3, 6)
+
+STEPS = 5001
 TEST_EVERY = 250
-CLASSES = (2, 4, 7, 8)
+
+START_STEPSIZE = 0.01
+UPDATE_SZ_EVERY = 1000
+SZ_FACTOR = 0.5
+
+START_ALPHA = 1
+UPDATE_ALPHA_EVERY = 3000
+ALPHA_FACTOR = 0.5
 
 
 def circuit(params, data):
     qml.templates.embeddings.AngleEmbedding(
             features=data, wires=range(WIRES), rotation="X"
-        )  
+        )
 
-    for layer in range(LAYERS): 
+    for layer in range(LAYERS):
         for wire in range(WIRES):
             qml.RX(params[layer][wire][0], wires=wire)
             qml.RY(params[layer][wire][1], wires=wire)
@@ -35,24 +45,37 @@ def circuit(params, data):
     return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1))
 
 
-def triplet_loss(params, qNode, anchor, positive, negative):
+def triplet_loss(params, qNode, anchor, positive, negative, alpha):
     a_value = qNode(params, anchor)
     p_value = qNode(params, positive)
     n_value = qNode(params, negative)
     # print(a_value, p_value, n_value)
-    return max(np.linalg.norm(a_value-p_value)**2 - np.linalg.norm(a_value - n_value)**2 + ALPHA, 0.0)
+    return max((np.linalg.norm(a_value-p_value)**2 -
+               np.linalg.norm(a_value - n_value)**2 + alpha),
+               0.0)
 
 
 def train():
     dev = qml.device('default.qubit', wires=WIRES, shots=None)
     qNode = qml.QNode(func=circuit, device=dev)
 
-    optimizer = qml.AdamOptimizer(stepsize=0.005)
-    def cost_fn(params):
-        return triplet_loss(params, qNode, anchor, positive, negative)
-    params = np.random.uniform(low=-np.pi, high=np.pi, size=(LAYERS, WIRES, 2))
+    stepsize = START_STEPSIZE
+    optimizer = qml.AdamOptimizer(stepsize)
 
-    data = load_data("mnist", shuffle=SEED, train_size=TRAIN_SIZE, test_size=TEST_SIZE, classes=CLASSES)
+    def cost_fn(params):
+        return triplet_loss(params, qNode, anchor, positive, negative, alpha)
+
+    params = np.random.uniform(low=-np.pi, high=np.pi, size=(LAYERS, WIRES, 2))
+    alpha = START_ALPHA
+
+    data = load_data("mnist", shuffle=SEED,
+                     train_size=TRAIN_SIZE,
+                     test_size=TEST_SIZE,
+                     classes=CLASSES)
+    occurences_train = [np.argmax(x) for x in data.train_target]
+    occurences_test = [np.argmax(x) for x in data.test_target]
+    print("Train", Counter(occurences_train))
+    print("Test", Counter(occurences_test))
 
     images = {}
     for cls in range(len(CLASSES)):
@@ -61,48 +84,68 @@ def train():
     for index, label in enumerate(data.train_target):
         images[np.argmax(label)].append(data.train_data[index])
 
-    for s in range(STEPS):
+    dbis = []
+
+    for step in range(STEPS):
         pos, neg = random.sample(range(len(CLASSES)), 2)
-        
+
         anchor, positive = random.sample(images[pos], 2)
         negative = random.choice(images[neg])
 
         params, c = optimizer.step_and_cost(cost_fn, params)
 
-        print(s, c)
+        print(f"step {step:{len(str(STEPS))}}| cost {c:8.5f}")
 
-        if s % TEST_EVERY == 0:
-            plot_test_set(data, qNode, params, s)
+        if step % TEST_EVERY == 0:
+            dbi = evaluate(data, qNode, params, step)
+            dbis.append(dbi)
+
+        if (step+1) % UPDATE_SZ_EVERY == 0:
+            stepsize *= SZ_FACTOR
+            optimizer.stepsize = stepsize
+            print("Updated stepsize to", stepsize)
+
+        if (step+1) % UPDATE_ALPHA_EVERY == 0:
+            alpha *= ALPHA_FACTOR
+            print("Updated alpha to", alpha)
+
+    print("DBIs:\n", dbis)
+    print("Minimum:", min(dbis))
 
 
-def plot_test_set(data, qNode, params, s):
-    colors = ["darkred", "blue", "green", "orange", "black", "lightblue", "lightgreen"]
-    images = {}
-    plt.rcParams["figure.figsize"] = (6,6)
+def evaluate(data, qNode, params, step, show=False, save=True):
 
+    # this will store:
+    # label (0 - len(CLASSES)), x_output, y_output, distance_to_center
+    values = np.zeros((len(data.test_target), 4))
+
+    # store label and output
+    for index, (label, datum) in enumerate(zip(data.test_target, data.test_data)):
+        output = qNode(params, datum)
+        values[index, 0] = np.argmax(label)
+        values[index, 1] = output[0]
+        values[index, 2] = output[1]
+
+    # calculate centers, key is label
+    centers = {}
     for cls in range(len(CLASSES)):
-        images[cls] = []
+        rows = np.where(values[:, 0] == cls)
+        center = np.average(values[rows][:, 1:3], axis=0)
+        centers[cls] = center
 
-    for label, datum in zip(data.test_target, data.test_data):
-        values = qNode(params, datum)
-        images[np.argmax(label)].append(values)
+    # calculate distance to center
+    for i in range(len(data.test_target)):
+        values[i, 3] = np.linalg.norm(values[i, 1:3] - centers[int(values[i, 0])])
 
-    for index, (cls, values) in enumerate(images.items()):
-        x = []
-        y = []
-        for value in values:
-            x.append(value[0])
-            y.append(value[1])
+    c_distances = distances_between_centers(centers)
+    print("Distances between centers\n", c_distances)
 
-        plt.scatter(x, y, color=colors.pop(0), alpha=0.6, label=str(CLASSES[index]))
-    
-    plt.title("step " + str(s))
-    plt.ylim(-1, 1)
-    plt.xlim(-1, 1)
-    plt.legend()
-    # plt.show()
-    plt.savefig("./images/" + str(s) + ".png")
-    plt.clf()
+    dbi = davies_bouldin_index(len(CLASSES), values, c_distances)
+    print("Davies Bouldin Index:", dbi)
+
+    plot(CLASSES, values, centers, step, show, save)
+
+    return dbi
 
 
 if __name__ == "__main__":
